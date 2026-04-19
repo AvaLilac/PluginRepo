358ecf55da48d65b8ffabc26de2a55764de6af1f
AvaLilac <257690424+AvaLilac@users.noreply.github.com>
2026-04-18 21:05:33 -0400

    Update themerepobackend.js

--- themebackend/themerepobackend.js
+++ themebackend/themerepobackend.js
@@ -7,6 +7,10 @@
     {
       "name": "Raptor's Themes",
       "url": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/themesbackend.js"
+    },
+    {
+      "name": "Amelia's Themes",
+      "url": "https://codeberg.org/api/v1/repos/Amelinyaa/aviaStuff/raw/themesbackend.js"
     }
   ]
 }
